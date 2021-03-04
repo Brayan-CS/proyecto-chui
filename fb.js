e3350fdf6129d7a9652207c4085e9297c1693867
= <=>
2021-03-03 22:41:24 -0500

    arreglo fb

--- fb.js
+++ fb.js
@@ -26,9 +26,9 @@ function guardarForm(e){
     function saveContactForm(data) {
         rootRef.push(data) // Hacemos referencia al método database de el SDK y hacemos referencia el nombre del objeto que contendrá nuestros registros y empujamos los nuevos envios de datos
           .then(function(){
-            alert('mensaje guardado'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
+            alert('Mensaje Guardado'); // Si la petición es correcta y almaceno los datos mostramos un mensaje al usuario.
           })
           .catch(function(){
-            alert('mensaje No guardado'); // En caso de ocurrir un error le mostramos al usuario que ocurrió un error.
+            alert('Mensaje No Guardado'); // En caso de ocurrir un error le mostramos al usuario que ocurrió un error.
           });
       };
